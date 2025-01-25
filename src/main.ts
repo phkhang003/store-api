56c0865668c6a904746a33cf3aee27a235c05d1f
LAPTOP-G4JJ8UTM\Phan_Hoang_Khang <KhangPhan1121>
2025-01-25 07:48:14 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,7 @@ async function bootstrap() {
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/swagger', app, document);
 
   const port = process.env.PORT || 3000;
   const isDev = process.env.NODE_ENV !== 'production';
