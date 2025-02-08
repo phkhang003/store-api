5ed2497725bdad38aeba19711ae1aa47cf9518ba
LAPTOP-G4JJ8UTM\Phan_Hoang_Khang <KhangPhan1121>
2025-02-08 08:40:52 +0700

    vercel

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../src/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 let app;
 
@@ -8,8 +9,16 @@ async function bootstrap() {
   if (!app) {
     app = await NestFactory.create(AppModule);
     app.enableCors();
-    app.setGlobalPrefix('api');
+    app.setGlobalPrefix('api', {
+      exclude: ['/'],
+    });
     app.useGlobalPipes(new ValidationPipe());
+    
+    // Thêm route chuyển hướng
+    app.getHttpAdapter().get('/', (req: Request, res: Response) => {
+      res.redirect(301, '/api/swagger');
+    });
+    
     await app.init();
   }
   return app;
